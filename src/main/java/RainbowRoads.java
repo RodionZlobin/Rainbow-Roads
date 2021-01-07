c5eb6be6cdc31037ae90913e4d8a3fc0dd8bbe1f
Rodion Zlobin <rodion.zlobin@so4it.com>
2021-01-07 06:05:16 +0100

    rainbow roads project upload

---
+++ src/main/java/RainbowRoads.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.util.Queue;
+import java.util.*;
+
+public class RainbowRoads {
+    public static void main(String[] args) {
+        HashMap<Integer, HashMap<Integer, Integer>> allStations = new HashMap<>();
+        HashMap<Integer, Boolean> rejectedStations = new HashMap<>();
+        Queue<Point> stationsQueue = new LinkedList<>();
+
+        Kattio scan = new Kattio(System.in);
+
+        int stationsAmount = scan.getInt();
+
+        for (int i = 1; i <= stationsAmount; i++) {
+            allStations.put(i, new HashMap<>());
+        }
+
+        while (scan.hasMoreTokens()) {
+            int stationFrom = scan.getInt();
+            int stationDestination = scan.getInt();
+            int edgeColor = scan.getInt();
+
+            allStations.get(stationFrom).put(stationDestination, edgeColor);
+            allStations.get(stationDestination).put(stationFrom, edgeColor);
+        }
+
+        allStations.forEach((station, stationDescription) -> {
+            if (!rejectedStations.containsKey(station)) {
+                HashMap<Integer, Boolean> checkedStations = new HashMap<>();
+                Set<Integer> duplicatedColors = new HashSet<>();
+                Set<Integer> readColors = new HashSet<>();
+
+                stationDescription.values().forEach(value ->
+                {
+                    if (readColors.contains(value)) {
+                        duplicatedColors.add(value);
+                    } else {
+                        readColors.add(value);
+                    }
+                });
+
+                stationDescription.keySet().stream()
+                        .filter(o -> duplicatedColors.contains(stationDescription.get(o)))
+                        .forEach(o -> stationsQueue.offer(new Point(o, stationDescription.get(o))));
+
+                checkedStations.put(station, true);
+
+                while (!stationsQueue.isEmpty()) {
+                    Point onHandle = stationsQueue.poll();
+                    rejectedStations.put(onHandle.x, true);
+
+                    for (int key : allStations.get(onHandle.x).keySet()) {
+                        if (allStations.get(onHandle.x).get(key) == onHandle.y && !checkedStations.containsKey(key)) {
+                            System.out.println(0);
+                            System.exit(0);
+                        }
+
+                        if (key != station && !rejectedStations.containsKey(key)) {
+                            stationsQueue.offer(new Point(key, allStations.get(onHandle.x).get(key)));
+                            checkedStations.put(onHandle.x, true);
+                        }
+                    }
+                }
+            }
+        });
+
+        TreeSet<Integer> result = new TreeSet<>();
+        allStations.keySet().stream()
+                .filter(key -> !rejectedStations.containsKey(key))
+                .forEach(result::add);
+
+        System.out.println(result.size());
+        result.forEach(System.out::println);
+    }
+}
